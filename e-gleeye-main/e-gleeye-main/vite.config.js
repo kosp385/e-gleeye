777c9473b008c95cd4c1e2794de04d47f39170bd
kosp385 <kosp385@gmail.com>
2026-03-05 10:01:21 +0900

    Delete e-gleeye-main/e-gleeye-main directory

--- e-gleeye-main/e-gleeye-main/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/e-gleeye/', // GitHub 저장소 이름과 동일하게 설정
-})
